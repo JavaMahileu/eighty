6116282af1c2b837364f4dff09193bc9dbb8c7e0
Roman Konovalov <Roman_Konovalov@epam.com>
2015-02-25 15:00:15 +0300

    Fix pmd

--- src/main/java/com/epam/eighty/exception/ExceptionsHandler.java
+++ src/main/java/com/epam/eighty/exception/ExceptionsHandler.java
@@ -11,19 +11,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionsHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionsHandler.class);
 
     @ExceptionHandler(DataAccessException.class)
     public void handleDataAccessException(final DataAccessException ex) {
-        final Logger log = LoggerFactory.getLogger(ExceptionsHandler.class);
-        log.error("DataAccessException: " + ex.getMessage());
-
+        LOG.error("DataAccessException: " + ex.getMessage());
     }
 
     @ExceptionHandler(IOException.class)
     public void handleIOException(final IOException ex) {
-        final Logger log = LoggerFactory.getLogger(ExceptionsHandler.class);
-        log.error("IOException: " + ex.getMessage());
-
+        LOG.error("IOException: " + ex.getMessage());
     }
 
 }
