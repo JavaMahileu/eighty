8c8f408e516f991aaa8db9df81b35103acbd4427
SviatlanaHlukhava <Sviatlana_Hlukhava@epam.com>
2015-02-26 05:53:48 +0000

    Set defaultTimeoutInterval = 60000 for e2e tests

--- src/test/javascript/protractor.conf.js
+++ src/test/javascript/protractor.conf.js
@@ -38,6 +38,6 @@ exports.config = {
         // If true, include stack traces in failures.
         includeStackTrace : true,
         // Default time to wait in ms before a test fails.
-        defaultTimeoutInterval : 50000
+        defaultTimeoutInterval : 60000
     }
 };
