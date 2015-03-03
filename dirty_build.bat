620117722550feb3f48a7c78486d5d93549e7d57
SviatlanaHlukhava <Sviatlana_Hlukhava@epam.com>
2015-03-03 10:15:38 +0000

    Update intent of using

--- dirty_build.bat
+++ dirty_build.bat
@@ -1,6 +1,6 @@
 rem test: no
 rem code quality checks: no
 rem deploy: no deploy
-rem intent of using: development
+rem intent of using: development without deploy
 rem minification: no
 mvn clean package -DskipTests -DskipITs -Dfindbugs.skip=true -Dpmd.skip=true -Dcheckstyle.skip=true
--- dirty_deploy.bat
+++ dirty_deploy.bat
@@ -1,6 +1,6 @@
 rem test: no
 rem code quality checks: no
 rem deploy: deploy on local server tomcat 7
-rem intent of using: development
+rem intent of using: development with deploy on local server tomcat 7 that should be started before deploy
 rem minification: no
 mvn clean package tomcat7:deploy -DskipTests -DskipITs -Dfindbugs.skip=true -Dpmd.skip=true -Dcheckstyle.skip=true
