29cd86e72701f0d413053076f5817de2fd3de206
Roman Konovalov <Roman_Konovalov@epam.com>
2015-03-12 11:26:57 +0300

    Add argument description

--- commit_build.bat
+++ commit_build.bat
@@ -3,4 +3,5 @@ rem code quality checks: yes (findbugs, pmd, checkstyle)
 rem deploy: no deploy
 rem intent of using: production
 rem minification: yes
+rem we can modify server port for protractor e2e tests by passing argument "-Dprotractor.port=_port_number_"
 mvn clean verify -Pprod %1
