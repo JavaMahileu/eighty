735f0d2d2c39ab026ddf06204cd10438f49ab2f7
Roman Konovalov <Roman_Konovalov@epam.com>
2015-03-19 12:40:51 +0300

    Fix DBPopulatorController

--- src/main/java/com/epam/eighty/web/api/DBPopulatorController.java
+++ src/main/java/com/epam/eighty/web/api/DBPopulatorController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,12 @@ public class DBPopulatorController {
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 500, message = "Request processing failed") })
     @Timed
     @RequestMapping(value = "/reload", method = RequestMethod.GET)
+    @Caching(evict = {
+            @CacheEvict(value = "topic", allEntries = true),
+            @CacheEvict(value = "question", allEntries = true),
+            @CacheEvict(value = "tag", allEntries = true),
+            @CacheEvict(value = "customer", allEntries = true)
+        })
     public void reloadDataBase(final HttpServletResponse response) throws IOException {
         dbPopulatorService.clean();
         dbPopulatorService.populate();
