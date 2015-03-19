f1b55d2934d9b6b6c8fcd5a11bc06cdb44324d04
Roman Konovalov <Roman_Konovalov@epam.com>
2015-03-19 12:10:19 +0300

    Remove grunt connect task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,8 @@
 'use strict';
 
-var mountFolder = function (connect, dir) {
-    return connect.static(require('path').resolve(dir));
-};
-
-var fillTopics = function (data) {
-    if (data.topics) {
-        data.topics.forEach(function (entry) {
-            entry.id = entry.id || 9999;
-            entry.topics = entry.topics || [];
-            entry.questions = entry.questions || [];
-        });
-    }
-};
-
-var restMiddleware = function (req, res, next) {
-    if (req.method === 'PUT') {
-        var body = {};
-        req.on('data', function (chunk) {
-            body = JSON.parse(chunk.toString());
-            fillTopics(body);
-        });
-        req.on('end', function () {
-            res.writeHead(200, 'OK', {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(body));
-        });
-    } else if (req.method === 'DELETE') {
-        res.writeHead(200, 'OK');
-        res.end();
-    } else {
-        return next();
-    }
-
-};
-
 module.exports = function (grunt) {
     
-    var protractorBrowserPort = grunt.option('p:port') || '8282';
+    var tomcatPort = grunt.option('p:port') || '8282';
 
     grunt.initConfig({
 
@@ -101,23 +67,6 @@ module.exports = function (grunt) {
                 singleRun: true
             }
         },
-        connect: {
-            livereload: {
-                options: {
-                    //keepalive: true,
-                    port: protractorBrowserPort,
-                    base: ['src/main/webapp', 'src/test/resources/json/root', 'src/test/resources/json'],
-                    middleware: function (connect, options) {
-                        return [
-                            restMiddleware,
-                            mountFolder(connect, options.base[0]),
-                            mountFolder(connect, options.base[1]),
-                            mountFolder(connect, options.base[2])
-                        ];
-                    }
-                }
-            }
-        },
         copy: {
             lcov: {
                 expand: true,
@@ -167,7 +116,7 @@ module.exports = function (grunt) {
                 options : {
                     configFile : 'src/test/javascript/protractor.conf.js',
                     args : {
-                        baseUrl : 'http://localhost:' + protractorBrowserPort
+                        baseUrl : 'http://localhost:' + tomcatPort
                     }
                 }
             },
@@ -177,7 +126,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-ngdocs');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
@@ -189,7 +137,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-protractor-runner');
 
     grunt.registerTask('minify', ['clean:jsmin', 'removelogging', 'uglify', 'clean:cssmin', 'cssmin']);
-    grunt.registerTask('p:test', ['clean:e2etests','processhtml:e2eTests', 'connect', 'protractor', 'processhtml:production']);
+    grunt.registerTask('p:test', ['clean:e2etests','processhtml:e2eTests', 'protractor', 'processhtml:production']);
     grunt.registerTask('k:test', ['processhtml:development', 'karma', 'copy:lcov']);
 
 };
